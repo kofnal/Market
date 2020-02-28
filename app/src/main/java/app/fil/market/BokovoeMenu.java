07116095a11aaaee75cd89b9b237a13fb7d9c4bb
kofnal <6586980@tut.by>
2020-02-28 22:09:34 +0300

    9 Bokovoe menu gotovo

--- app/src/main/java/app/fil/market/BokovoeMenu.java
+++ app/src/main/java/app/fil/market/BokovoeMenu.java
@@ -1,10 +1,13 @@
 package app.fil.market;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,7 +24,7 @@ import android.widget.ImageView;
 
 import app.fil.market.korzina.KorzinaActivity;
 
-public class BokovoeMenu extends AppCompatActivity {
+public class BokovoeMenu extends AppCompatActivity   {
 
     private AppBarConfiguration mAppBarConfiguration;
     NavController navController;
@@ -43,17 +46,40 @@ public class BokovoeMenu extends AppCompatActivity {
         });
         final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+
 //        // Passing each menu ID as a set of Ids because each
 //        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_share, R.id.nav_send, R.id.nav_tovari)
+                R.id.nav_home,  R.id.nav_tovari)
                 .setDrawerLayout(drawer)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-
+        MenuItem menuItemTelefon = navigationView.getMenu().findItem(R.id.nav_telefon);
+        menuItemTelefon.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                System.out.println("ItemClickListener "+menuItem.toString());
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+getResources().getString(R.string.menu_share)));
+                startActivity(intent);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        MenuItem menuItemMail = navigationView.getMenu().findItem(R.id.nav_send);
+        menuItemMail.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto",getResources().getString(R.string.chetverg_mail), null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                return true;
+            }
+        });
 
     }
 
@@ -64,4 +90,5 @@ public class BokovoeMenu extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
